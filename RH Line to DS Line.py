87cdcebdd0b3af4a240c4d284e63321409b7cc90
Konrad K Sobon <ksobon1986@gmail.com>
2014-10-17 18:37:20 -0400

    Update RH Line to DS Line.py

--- RH Line to DS Line.py
+++ RH Line to DS Line.py
@@ -13,19 +13,15 @@ clr.AddReferenceToFileAndPath(RhinoCommonPath + r"\RhinoCommon.dll")
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
-from Autodesk.DesignScript.Geometry import *
 import Rhino as rc
+from Autodesk.DesignScript.Geometry import *
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 rhObjects = IN[0]
 
-#3dPoint Conversion
+#3dPoint Conversion function
 def rhPoint3dToPoint(rhPoint):
-	try:
-		rhPoint = rhPoint.Geometry
-	except:
-		pass
 	rhPointX = rhPoint.X
 	rhPointY = rhPoint.Y
 	rhPointZ = rhPoint.Z
@@ -34,10 +30,6 @@ def rhPoint3dToPoint(rhPoint):
 
 #LineCurve conversion function
 def rhLineToLine(rhCurve):
-	try:
-		rhCurve = rhCurve.Geometry
-	except:
-		pass
 	rhStartPoint = rhCurve.PointAtStart
 	dsStartPoint = rhPoint3dToPoint(rhStartPoint)
 	rhEndPoint = rhCurve.PointAtEnd
@@ -48,7 +40,12 @@ def rhLineToLine(rhCurve):
 #convert rhino/gh geometry to ds geometry
 dsLines = []
 for i in rhObjects:
-	dsLines.append(rhLineToLine(i))
+	try:
+		i = i.Geometry
+	except:
+		pass
+	if i.ToString() == "Rhino.Geometry.LineCurve":
+		dsLines.append(rhLineToLine(i))
 
 #Assign your output to the OUT variable
 OUT = dsLines
