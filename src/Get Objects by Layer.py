a64e6c83abb6269c674104a25a62379b6ed6643a
Konrad K Sobon <ksobon1986@gmail.com>
2014-10-29 22:49:45 -0400

    Update Get Objects by Layer.py

--- src/Get Objects by Layer.py
+++ src/Get Objects by Layer.py
@@ -5,15 +5,24 @@ import clr
 import sys
 clr.AddReference('ProtoGeometry')
 
-RhinoCommonPath = r'C:\Program Files\Rhinoceros 5 (64-bit)\System'
-if RhinoCommonPath not in sys.path:
-	sys.path.Add(RhinoCommonPath)
-clr.AddReferenceToFileAndPath(RhinoCommonPath + r"\RhinoCommon.dll")
-
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
+import pickle
+import os
+rhCommonFile = r'C:\Program Files\Rhinoceros 5 (64-bit)\System\RhinoCommon.dll'
+if os.path.exists(rhCommonFile):
+	RhinoCommonPath = r'C:\Program Files\Rhinoceros 5 (64-bit)\System'
+	if RhinoCommonPath not in sys.path:
+		sys.path.Add(RhinoCommonPath)
+	clr.AddReferenceToFileAndPath(RhinoCommonPath + r"\RhinoCommon.dll")
+	import Rhino as rc
+	message = None
+else:
+	message = "RhinoCommon.dll not located in \nC:\Program Files\Rhinoceros 5 \n(64-bit)\System. Please change \nRhinoCommonPath variable."
 
-import Rhino as rc
+from Autodesk.DesignScript.Geometry import *
+from System import Array
+from System.Collections.Generic import *
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
@@ -22,4 +31,7 @@ rhLayer = IN[1]
 
 #get objects by layer
 #Assign your output to the OUT variable
-OUT = rhModel.Objects.FindByLayer(rhLayer)
+if message != None:
+	OUT = '\n'.join('{:^35}'.format(s) for s in message.split('\n'))
+else:
+	OUT = rhModel.Objects.FindByLayer(rhLayer)
