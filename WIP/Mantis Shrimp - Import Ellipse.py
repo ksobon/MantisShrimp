2dd83236f8e422c3a3e30dd0ff99f6c13392873a
Konrad K Sobon <ksobon1986@gmail.com>
2014-12-07 20:29:23 -0500

    Update Mantis Shrimp - Import Ellipse.py

--- WIP/Mantis Shrimp - Import Ellipse.py
+++ WIP/Mantis Shrimp - Import Ellipse.py
@@ -2,7 +2,7 @@
 #@arch_laboratory, http://archi-lab.net
 
 """
-This component reads ellipses exported from Dynamo
+This component reads Ellipses exported from Dynamo
 
     Args:
         _filePath: path to file to read
@@ -14,21 +14,21 @@ ghenv.Component.NickName = 'DSEllipse'
 ghenv.Component.Category = "Mantis Shrimp"
 
 import sys
-ms_path = r'C:\\Users\\ksobon\\AppData\\Roaming\\Dynamo\\0.7\\packages\\Mantis Shrimp\\extra'
+ms_path = r'C:\Users\ksobon\AppData\Roaming\Dynamo\0.7\packages\Mantis Shrimp\extra'
 if ms_path not in sys.path:
     sys.path.append(ms_path)
 import mantisshrimp as ms
-mantis = mantisshrimp
+mantis = ms
 reload(mantis)
 
-import Rhino as rc
-import cPickle as pickle
 import os
+from System import Array
+from System.Collections.Generic import *
+import cPickle as pickle
+import Rhino as rc
 import Grasshopper.Kernel as gh
 from Grasshopper import DataTree
 from Grasshopper.Kernel.Data import GH_Path
-from System import Array
-from System.Collections.Generic import *
 
 class SerializeObjects(object):
 	def __init__(self, filePath, data = None):
@@ -47,8 +47,6 @@ serializer = SerializeObjects(_filePath)
 serializer.readFromFile()
 data = serializer.data
 
-# recursive function to process any input list and output 
-# matching structure list
 def process_list(_func, _list):
     return map( lambda x: process_list(_func, x) if type(x)==list else _func(x), _list )
 
@@ -56,10 +54,20 @@ def toRHObject(item):
     if type(item) == ms.MSEllipse:
         return item.toRHEllipse()
 
-if _import:
-    _tree = DataTree[rc.Geometry.Ellipse]()
-    some_data = process_list(toRHObject, data)
-    someList = Array[Array[rc.Geometry.Ellipse]](map(tuple, some_data))
-#    outEllipses = _tree.AddRange(someList)
+"""Transforms nestings of lists or tuples to a Grasshopper DataTree"""
+"""Big thanks to Giulio Piacentino for this function"""
+def list_to_tree(input, none_and_holes=False, source=[0]):
+    def proc(input,tree,track):
+        path = GH_Path(Array[int](track))
+        if len(input) == 0 and none_and_holes: tree.EnsurePath(path); return
+        for i,item in enumerate(input):
+            if hasattr(item, '__iter__'): #if list or tuple
+                track.append(i); proc(item,tree,track); track.pop()
+            else:
+                if none_and_holes: tree.Insert(item,path,i)
+                elif item is not None: tree.Add(item,path)
+    if input is not None: t=DataTree[object]();proc(input,t,source[:]);return t
 
-print(someList)
+if _import:
+    dataList = process_list(toRHObject, data)
+    outEllipse = list_to_tree(dataList)
