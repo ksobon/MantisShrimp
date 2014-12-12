221bc9654a5d441640081560b58cca50627c420a
Konrad K Sobon <ksobon1986@gmail.com>
2014-12-12 17:08:54 -0500

    Update exportToDS.py

--- WIP/exportToDS.py
+++ WIP/exportToDS.py
@@ -63,6 +63,37 @@ class SerializeObjects(object):
         with open(self.filePath, 'rb') as inf:
             self.data = pickle.load(inf)
 
+def rhLineCurveToMSLine(item):
+    msStartPt = MSPoint(item.PointAtStart.X, item.PointAtStart.Y, item.PointAtStart.Z)
+    msEndPt = MSPoint(item.PointAtEnd.X, item.PointAtEnd.Y, item.PointAtEnd.Z)
+    return MSLine(msStartPt, msEndPt)
+
+def rhPolyLineToMSPolyLine(item):
+    segments = []
+    for i in range(0, item.SegmentCount,1):
+        line = item.SegmentAt(i)
+        msStartPt = MSPoint(line.PointAtStart.X, line.PointAtStart.Y, line.PointAtStart.Z)
+        msEndPt = MSPoint(line.PointAtEnd.X, line.PointAtEnd.Y, line.PointAtEnd.Z)
+        segments.append(MSLine(msStartPt, msEndPt))
+    return MSPolyLine(segments)
+
+def rhArcCurveToMSArc(item):
+    item = item.TryGetArc()[1]
+    startPoint = MSPoint(item.StartPoint.X, item.StartPoint.Y, item.StartPoint.Z)
+    midPoint = MSPoint(item.MidPoint.X, item.MidPoint.Y, item.MidPoint.Z)
+    endPoint = MSPoint(item.EndPoint.X, item.EndPoint.Y, item.EndPoint.Z)
+    return MSArc(startPoint, midPoint, endPoint)
+
+def rhNurbsCurveToMSNurbsCurve(item):
+    msControlPoints = []
+    for pt in item.Points:
+        msControlPoints.append(MSPoint4d(pt.Location.X, pt.Location.Y, pt.Location.Z, pt.Weight))
+    knots = []
+    for i in range(0, item.Knots.Count, 1):
+        knots.append(item.Knots[i])
+    # weights information is stored in control points
+    weights = None
+    return MSNurbsCurve(msControlPoints, weights, knots, item.Degree)
 
 geometryOut = []
 if _export:
@@ -70,10 +101,12 @@ if _export:
         if type(item) == rc.Geometry.Point3d:
             msPoint = MSPoint(item.X, item.Y, item.Z)
             geometryOut.append(msPoint)
+
         elif type(item) == rc.Geometry.LineCurve:
             msStartPt = MSPoint(item.PointAtStart.X, item.PointAtStart.Y, item.PointAtStart.Z)
             msEndPt = MSPoint(item.PointAtEnd.X, item.PointAtEnd.Y, item.PointAtEnd.Z)
             geometryOut.append(MSLine(msStartPt, msEndPt))
+
         elif type(item) == rc.Geometry.PolyCurve and item.IsPolyline() == True:
             segments = []
             for i in range(0, item.SegmentCount,1):
@@ -82,25 +115,29 @@ if _export:
                 msEndPt = MSPoint(line.PointAtEnd.X, line.PointAtEnd.Y, line.PointAtEnd.Z)
                 segments.append(MSLine(msStartPt, msEndPt))
             geometryOut.append(MSPolyLine(segments))
+
         elif type(item) == rc.Geometry.NurbsCurve and item.IsEllipse() == True:
             item = item.TryGetEllipse()[1]
             msVector = MSVector(item.Plane.Normal.X,item.Plane.Normal.Y, item.Plane.Normal.Z) 
             msOrigin = MSPoint(item.Plane.Origin.X, item.Plane.Origin.Y, item.Plane.Origin.Z)
             msPlane = MSPlane(msOrigin, msVector)
             geometryOut.append(MSEllipse(msPlane, item.Radius1, item.Radius2))
+
         elif type(item) == rc.Geometry.ArcCurve and item.IsCircle() == True:
             item = item.TryGetCircle()[1]
             msVector = MSVector(item.Normal.X,item.Normal.Y, item.Normal.Z)
             msOrigin = MSPoint(item.Center.X, item.Center.Y, item.Center.Z)
             msPlane = MSPlane(msOrigin, msVector)
             geometryOut.append(MSCircle(msPlane, item.Radius))
+
         elif type(item) == rc.Geometry.ArcCurve and item.IsArc() == True:
             item = item.TryGetArc()[1]
             startPoint = MSPoint(item.StartPoint.X, item.StartPoint.Y, item.StartPoint.Z)
             midPoint = MSPoint(item.MidPoint.X, item.MidPoint.Y, item.MidPoint.Z)
             endPoint = MSPoint(item.EndPoint.X, item.EndPoint.Y, item.EndPoint.Z)
             geometryOut.append(MSArc(startPoint, midPoint, endPoint))
-        elif type(item) == rc.Geometry.NurbsCurve and item.SpanCount == 1:
+
+        elif type(item) == rc.Geometry.NurbsCurve:
             msControlPoints = []
             for pt in item.Points:
                 msControlPoints.append(MSPoint4d(pt.Location.X, pt.Location.Y, pt.Location.Z, pt.Weight))
@@ -110,6 +147,74 @@ if _export:
             # weights information is stored in control points
             weights = None
             geometryOut.append(MSNurbsCurve(msControlPoints, weights, knots, item.Degree))
+#PolyCurve
+        elif type(item) == rc.Geometry.PolyCurve:
+            msSubCurves = []
+            segmentCount = item.SegmentCount
+            for i in range(0, segmentCount):
+                curve = item.SegmentCurve(i)
+                if type(curve) == rc.Geometry.LineCurve:
+                    msSubCurves.append(rhLineCurveToMSLine(curve))
+                elif type(curve) == rc.Geometry.PolyCurve and curve.IsPolyLine() == True:
+                    msSubCurves.append(rhPolyLineToMSPolyLine(curve.TryGetPolyline()[1]))
+                elif type(curve) == rc.Geometry.PolylineCurve and curve.IsPolyline() == True:
+                    msSubCurves.append(curve.TryGetPolyline()[1])
+                elif type(curve) == rc.Geometry.ArcCurve and curve.IsArc() == True:
+                    msSubCurves.append(rhArcCurveToMSArc(curve))
+                elif type(curve) == rc.Geometry.NurbsCurve:
+                    msSubCurves.append(rhNurbsCurveToMSNurbsCurve(curve))
+            msPolyCurve = MSPolyCurve(msSubCurves)
+            geometryOut.append(msPolyCurve)
+
+        elif type(item) == rc.Geometry.Mesh:
+            msFaces = []
+            for i in range(0, item.Faces.Count):
+                face = item.Faces.Item[i]
+                if face.IsQuad:
+                    msFaces.append(MSMeshFace(face.A, face.B, face.C, face.D))
+                else:
+                    msFaces.append(MSMeshFace(face.A, face.B, face.C))
+            msPoints = []
+            for i in range(0, item.Vertices.Count, 1):
+                msPoints.append(MSPoint(item.Vertices.Item[i].X, item.Vertices.Item[i].Y, item.Vertices.Item[i].Z))
+            geometryOut.append(MSMesh(msPoints, msFaces))
+
+        elif type(item) == rc.Geometry.Brep and item.IsSurface == True:
+            item = item.Faces[0].ToNurbsSurface()
+            msControlPoints = []
+            for pt in item.Points:
+                msControlPoints.append(MSPoint4d(pt.Location.X, pt.Location.Y, pt.Location.Z, pt.Weight))
+            # since weights in Rhino are stored in points 
+            # they will be extracted from MSPoint4d
+            weights = None
+            knotsU = []
+            for i in range(0, item.KnotsU.Count, 1):
+                knotsU.append(item.KnotsU[i])
+            knotsV = []
+            for i in range(0, item.KnotsV.Count, 1):
+                knotsV.append(item.KnotsV[i])
+            geometryOut.append(MSNurbsSurface(msControlPoints, weights, knotsU, knotsV, item.OrderU, item.OrderV, item.SpanCount(0), item.SpanCount(1), item.IsRational))
+
+        else:
+            geometryOut.append(MSData(item))
+
+    try:
+        serializer = SerializeObjects(filePath, geometryOut)
+        serializer.saveToFile()
+        warnType = gh.GH_RuntimeMessageLevel.Remark
+        msg = "File is exported to " + filePath + "Now you can use Dynamo to import the file."
+    except Exception, e:
+        warnType = gh.GH_RuntimeMessageLevel.Warning
+        msg = "Failed to export: \n" + `e`
+
+else:
+    msg = "Export set to false."
+
+ghenv.Component.AddRuntimeMessage(warnType, msg)
+
+print(geometryOut)
+
+"""
         elif type(item) == rc.Geometry.NurbsCurve and item.SpanCount > 1:
             rhSubCurve, msPolyCurves = [], []
             for i in range(0, item.SpanCount, 1):
@@ -126,48 +231,4 @@ if _export:
                 weights = None
                 msPolyCurves.append(MSNurbsCurve(msControlPoints, weights, knots, curve.Degree))
             geometryOut.append(MSPolyCurve(msPolyCurves))
-#        elif type(item) == rc.Geometry.PolyCurve:
-#            
-        elif type(item) == rc.Geometry.Mesh:
-            msFaces = []
-            for i in range(0, item.Faces.Count, 1):
-                face = item.Faces.Item[i]
-                if face.IsQuad:
-                    msFaces.append(MSMeshFace(face.A, face.B, face.C, face.D))
-                else:
-                    msFaces.append(MSMeshFace(face.A, face.B, face.C))
-            msPoints = []
-            for i in range(0, item.Vertices.Count, 1):
-                msPoints.append(MSPoint(item.Vertices.Item[i].X, item.Vertices.Item[i].Y, item.Vertices.Item[i].Z))
-            geometryOut.append(MSMesh(msPoints, msFaces))
-        elif type(item) == rc.Geometry.Brep and item.IsSurface == True:
-            item = item.Faces[0].ToNurbsSurface()
-            msControlPoints = []
-            for pt in item.Points:
-                msControlPoints.append(MSPoint4d(pt.Location.X, pt.Location.Y, pt.Location.Z, pt.Weight))
-            # since weights in Rhino are stored in points 
-            # they will be extracted from MSPoint4d
-            weights = None
-            knotsU = []
-            for i in range(0, item.KnotsU.Count, 1):
-                knotsU.append(item.KnotsU[i])
-            knotsV = []
-            for i in range(0, item.KnotsV.Count, 1):
-                knotsV.append(item.KnotsV[i])
-            geometryOut.append(MSNurbsSurface(msControlPoints, weights, knotsU, knotsV, item.OrderU, item.OrderV, item.SpanCount(0), item.SpanCount(1), item.IsRational))
-        else:
-            geometryOut.append(MSData(item))
-    try:
-        serializer = SerializeObjects(filePath, geometryOut)
-        serializer.saveToFile()
-        warnType = gh.GH_RuntimeMessageLevel.Remark
-        msg = "File is exported to " + filePath + "Now you can use Dynamo to import the file."
-    except Exception, e:
-        warnType = gh.GH_RuntimeMessageLevel.Warning
-        msg = "Failed to export: \n" + `e`
-else:
-    msg = "Export set to false."
-
-ghenv.Component.AddRuntimeMessage(warnType, msg)
-
-print(geometryOut)
+"""
