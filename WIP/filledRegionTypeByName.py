9db1ba3d6408a7d2e7eaa74e4bda3705ce179b08
Konrad K Sobon <ksobon1986@gmail.com>
2014-12-23 17:48:34 -0500

    Create filledRegionTypeByName.py

---
+++ WIP/filledRegionTypeByName.py
@@ -0,0 +1,41 @@
+import clr
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+from System.Collections.Generic import *
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+# Import ToDSType(bool) extension method
+clr.AddReference("RevitNodes")
+import Revit
+clr.ImportExtensions(Revit.Elements)
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+_name = IN[0]
+
+collector = FilteredElementCollector(doc).OfClass(FilledRegionType).ToElements()
+
+test = []
+for i in collector:
+	if i.ToDSType(True).Name == _name:
+		test.append(i)
+	else:
+		continue
+#Assign your output to the OUT variable
+OUT = test
