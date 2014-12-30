3131a25100cc5e839f8b572563cd8449690fb8a8
Konrad K Sobon <ksobon1986@gmail.com>
2014-12-30 13:46:30 -0500

    Update Get Rhino File.py

--- Dynamo/src/Get Rhino File.py
+++ Dynamo/src/Get Rhino File.py
@@ -14,7 +14,7 @@ msPath = appDataPath + r"\Dynamo\0.7\packages\Mantis Shrimp\extra"
 if msPath not in sys.path:
 	sys.path.Add(msPath)
 
-possibleRhPaths, message = [], None
+possibleRhPaths = []
 possibleRhPaths.append(r"C:\Program Files\Rhinoceros 5 (64-bit)\System\RhinoCommon.dll")
 possibleRhPaths.append(r"C:\Program Files\Rhinoceros 5.0 (64-bit)\System\RhinoCommon.dll")
 possibleRhPaths.append(r"C:\Program Files\McNeel\Rhinoceros 5.0\System\RhinoCommon.dll")
@@ -24,9 +24,6 @@ for i, j in zip(possibleRhPaths, checkPaths):
 	if j and i not in sys.path:
 		sys.path.Add(i)
 		clr.AddReferenceToFileAndPath(i)
-		break
-	else:
-		message = "Please provide a valid path to RhinoCommon.dll"
 
 from Autodesk.DesignScript.Geometry import *
 import Rhino as rc
@@ -37,7 +34,4 @@ path = str(IN[0])
 
 #read 3dm file
 #Assign your output to the OUT variable
-if message != None:
-	OUT = '\n'.join('{:^35}'.format(s) for s in message.split('\n'))
-else:
-	OUT = rc.FileIO.File3dm.Read(path)
+OUT = rc.FileIO.File3dm.Read(path)
