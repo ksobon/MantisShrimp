c707b7605549e8144d3bbc0e7d901631857442ab
Konrad K Sobon <ksobon1986@gmail.com>
2015-04-29 11:08:09 -0400

    Update RH Arc to DS Arc.py

--- Dynamo/src/RH Arc to DS Arc.py
+++ Dynamo/src/RH Arc to DS Arc.py
@@ -4,6 +4,7 @@
 import clr
 import sys
 clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
 
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
