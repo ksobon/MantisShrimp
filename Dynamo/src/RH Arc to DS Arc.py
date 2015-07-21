ec05ebe3df340fa5c01c4eba3656d2de4bf1c880
Konrad K Sobon <ksobon1986@gmail.com>
2015-07-21 16:59:17 -0400

    Update RH Arc to DS Arc.py

--- Dynamo/src/RH Arc to DS Arc.py
+++ Dynamo/src/RH Arc to DS Arc.py
@@ -4,21 +4,17 @@
 import clr
 import sys
 clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
 
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
 appDataPath = os.getenv('APPDATA')
-msPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\extra"
-rhPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin"
-rhDllPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll"
+msPath = appDataPath + r'\Dynamo\0.8\packages\Mantis Shrimp\extra'
 if msPath not in sys.path:
-	sys.path.Add(msPath)
-if rhPath not in sys.path:
-	sys.path.Add(rhPath)
-	clr.AddReferenceToFileAndPath(rhDllPath)
+	sys.path.append(msPath)
+rhDllPath = appDataPath + r'\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll'
+clr.AddReferenceToFileAndPath(rhDllPath)
 
 from Autodesk.DesignScript.Geometry import *
 import Rhino as rc
@@ -48,6 +44,8 @@ def rhPlaneToPlane(rhPlane):
 	normal = rhVector3dToVector(rhPlane.Normal)
 	origin = rhPoint3dToPoint(rhPlane.Origin)
 	dsPlane = Plane.ByOriginNormal(origin, normal)
+	normal.Dispose()
+	origin.Dispose()
 	return dsPlane
 
 #arc conversion function
@@ -56,17 +54,29 @@ def rhArcToArc(rhArc):
 	dsEndPoint = rhPoint3dToPoint(rhArc.Arc.EndPoint)
 	dsCenter = rhPoint3dToPoint(rhArc.Arc.Center)
 	dsArc = Arc.ByCenterPointStartPointEndPoint(dsCenter, dsStartPoint, dsEndPoint)
+	dsStartPoint.Dispose()
+	dsEndPoint.Dispose()
+	dsCenter.Dispose()
 	return dsArc
 
 #convert rhino/gh geometry to ds geometry
-dsArcs = []
-for i in rhObjects:
-	try:
-		i = i.Geometry
-	except:
-		pass
-	if i.ToString() == "Rhino.Geometry.ArcCurve" and i.IsArc():
-		dsArcs.append(rhArcToArc(i))
+try:
+	errorReport = None
+	dsArcs = []
+	for i in rhObjects:
+		try:
+			i = i.Geometry
+		except:
+			pass
+		if i.ToString() == "Rhino.Geometry.ArcCurve" and i.IsArc():
+			dsArcs.append(rhArcToArc(i))
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
 
 #Assign your output to the OUT variable
-OUT = dsArcs
+if errorReport == None:
+	OUT = dsArcs
+else:
+	OUT = errorReport
