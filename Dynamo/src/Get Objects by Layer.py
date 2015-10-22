29ac6e6d4f4d26ddeb00d9ecc256c4a9093effa2
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-22 19:43:55 -0400

    Update Get Objects by Layer.py
    handles single layer as well as multiple layers

--- Dynamo/src/Get Objects by Layer.py
+++ Dynamo/src/Get Objects by Layer.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
@@ -20,12 +20,38 @@ if rhPath not in sys.path:
 	sys.path.Add(rhPath)
 	clr.AddReferenceToFileAndPath(rhDllPath)
 
+import sys
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 rhModel = IN[0]
-rhLayer = IN[1]
 
-objects = rhModel.Objects.FindByLayer(rhLayer)
+if isinstance(IN[1], list):
+	rhLayer = IN[1]
+else:
+	rhLayer = [IN[1]]
+
+def GetObjectsByLayer(rhl = None, rhm = rhModel):
+	if rhl != None:
+		return rhm.Objects.FindByLayer(rhl)
+	else:
+		return None
+
+def ProcessList(_func, _list):
+    return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
+
+try:
+	errorReport = None
+	objects = ProcessList(GetObjectsByLayer, rhLayer)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
 
 #Assign your output to the OUT variable
-OUT = objects
+if errorReport == None:
+	OUT = objects
+else:
+	OUT = errorReport
