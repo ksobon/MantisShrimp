fb7d53c0acfd869644bb3fac78b5a6b73dceec2b
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-22 21:26:31 -0400

    Update RH Polyline to DS Polycurve.py

--- Dynamo/src/RH Polyline to DS Polycurve.py
+++ Dynamo/src/RH Polyline to DS Polycurve.py
@@ -1,9 +1,10 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
 import sys
 clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
 
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
@@ -24,7 +25,11 @@ import Rhino as rc
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
-rhObjects = IN[0]
+
+if isinstance(IN[0], list):
+	rhObjects = IN[0]
+else:
+	rhObjects = [IN[0]]
 
 #3dPoint Conversion function
 def rhPoint3dToPoint(rhPoint):
@@ -44,15 +49,28 @@ def rhCurveToPolyCurve(rhCurve):
 	del ptArray[:]
 	return dsPolyCurve
 
-#convert rhino/gh geometry to ds geometry
-dsPolyCurves = []
-for i in rhObjects:
+def GetPolylineCurve(rhObj):
 	try:
-		i = i.Geometry
+		geo = rhObj.Geometry
+		if geo.ToString() == "Rhino.Geometry.PolylineCurve":
+			return rhCurveToPolyCurve(geo)
 	except:
 		pass
-	if i.ToString() == "Rhino.Geometry.PolylineCurve":
-		dsPolyCurves.append(rhCurveToPolyCurve(i))
 
+def ProcessList(_func, _list):
+	return map(lambda x: ProcessList(_func, x) if type(x) == list else _func(x) , _list)
+
+#convert rhino/gh geometry to ds geometry
+try:
+	errorReport = None
+	dsPolyCurves = ProcessList(GetPolylineCurve, rhObjects)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+	
 #Assign your output to the OUT variable
-OUT = dsPolyCurves
+if errorReport == None:
+	OUT = dsPolyCurves
+else:
+	OUT = errorReport
