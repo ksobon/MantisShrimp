db1eb39975f4fe5b732d62ff42bde0193c250f1b
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-22 22:23:02 -0400

    Update RH Arc to DS Arc.py

--- Dynamo/src/RH Arc to DS Arc.py
+++ Dynamo/src/RH Arc to DS Arc.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
@@ -21,7 +21,11 @@ import Rhino as rc
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
-rhObjects = IN[0]
+
+if isinstance(IN[0], list):
+	rhObjects = IN[0]
+else:
+	rhObjects = [IN[0]]
 
 #Vector3d conversion function
 def rhVector3dToVector(rhVector):
@@ -59,22 +63,26 @@ def rhArcToArc(rhArc):
 	dsCenter.Dispose()
 	return dsArc
 
+def GetArc(rhObj):
+	try:
+		geo = rhObj.Geometry
+		if geo.ToString() == "Rhino.Geometry.ArcCurve" and geo.IsArc():
+			return rhArcToArc(geo)
+	except:
+		pass
+
+def ProcessList(_func, _list):
+	return map(lambda x: ProcessList(_func, x) if type(x) == list else _func(x) , _list)
+
 #convert rhino/gh geometry to ds geometry
 try:
 	errorReport = None
-	dsArcs = []
-	for i in rhObjects:
-		try:
-			i = i.Geometry
-		except:
-			pass
-		if i.ToString() == "Rhino.Geometry.ArcCurve" and i.IsArc():
-			dsArcs.append(rhArcToArc(i))
+	dsArcs = ProcessList(GetArc, rhObjects)
 except:
 	# if error accurs anywhere in the process catch it
 	import traceback
 	errorReport = traceback.format_exc()
-
+	
 #Assign your output to the OUT variable
 if errorReport == None:
 	OUT = dsArcs
