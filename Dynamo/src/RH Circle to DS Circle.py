bfa9acd01ab7ada83d2d7627cf8411f18313e886
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-22 22:53:48 -0400

    Update RH Circle to DS Circle.py

--- Dynamo/src/RH Circle to DS Circle.py
+++ Dynamo/src/RH Circle to DS Circle.py
@@ -21,7 +21,11 @@ import Rhino as rc
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
-rhObjects = IN[0]
+
+if isinstance(IN[0], list):
+	rhObjects = IN[0]
+else:
+	rhObjects = [IN[0]]
 
 #Vector3d conversion function
 def rhVector3dToVector(rhVector):
@@ -57,22 +61,27 @@ def rhCircleToCircle(rhCurve):
 	plane.Dispose()
 	return dsCircle
 
+def GetCircle(rhObj):
+	try:
+		geo = rhObj.Geometry
+		if geo.ToString() == "Rhino.Geometry.ArcCurve" and geo.IsCircle():
+			if geo.IsClosed and geo.IsRational:
+				return rhCircleToCircle(geo)
+	except:
+		pass
+
+def ProcessList(_func, _list):
+	return map(lambda x: ProcessList(_func, x) if type(x) == list else _func(x) , _list)
+
 #convert rhino/gh geometry to ds geometry
 try:
 	errorReport = None
-	dsCircles = []
-	for i in rhObjects:
-		try:
-			i = i.Geometry
-		except:
-			pass
-		if i.ToString() == "Rhino.Geometry.ArcCurve" and i.IsCircle():
-			dsCircles.append(rhCircleToCircle(i))
+	dsCircles = ProcessList(GetCircle, rhObjects)
 except:
 	# if error accurs anywhere in the process catch it
 	import traceback
 	errorReport = traceback.format_exc()
-
+	
 #Assign your output to the OUT variable
 if errorReport == None:
 	OUT = dsCircles
