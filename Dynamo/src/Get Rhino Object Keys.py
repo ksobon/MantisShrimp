9bdf3763cbdc129953fba6ab748d2dce841a4be4
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-23 21:38:56 -0400

    Update Get Rhino Object Keys.py

--- Dynamo/src/Get Rhino Object Keys.py
+++ Dynamo/src/Get Rhino Object Keys.py
@@ -1,39 +1,49 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
 import sys
-clr.AddReference('ProtoGeometry')
 
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
-appDataPath = os.getenv('APPDATA')
-msPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\extra"
-rhPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin"
-rhDllPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll"
-if msPath not in sys.path:
-	sys.path.append(msPath)
-if rhPath not in sys.path:
-	sys.path.append(rhPath)
-	clr.AddReferenceToFileAndPath(rhDllPath)
 
-from Autodesk.DesignScript.Geometry import *
+appDataPath = os.getenv('APPDATA')
+rhDllPath = appDataPath + r'\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll'
+clr.AddReferenceToFileAndPath(rhDllPath)
+
 import Rhino as rc
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
-rhObjects = IN[0]
 
-#convert rhino/gh geometry to ds geometry
-userStringKeys = []
-for item in rhObjects:
+if isinstance(IN[0], list):
+	rhObjects = IN[0]
+else:
+	rhObjects = [IN[0]]
+
+def GetObjectKeys(obj):
 	try:
-		item = item.Geometry
+		geo = obj.Geometry
+		return geo.GetUserStrings().Keys
 	except:
 		pass
-	userStringKeys.append(item.GetUserStrings().Keys)
+
+def ProcessList(_func, _list):
+	return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
+
+#extract object type
+try:
+	errorReport = None
+	rhKeys = ProcessList(GetObjectKeys, rhObjects)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
 
 #Assign your output to the OUT variable
-OUT = userStringKeys
+if errorReport == None:
+	OUT = rhKeys
+else:
+	OUT = errorReport
