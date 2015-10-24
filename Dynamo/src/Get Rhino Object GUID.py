90ab7afa700db26ae77cdc0cae5f9554575de013
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-23 21:35:35 -0400

    Update Get Rhino Object GUID.py

--- Dynamo/src/Get Rhino Object GUID.py
+++ Dynamo/src/Get Rhino Object GUID.py
@@ -1,35 +1,45 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
 import sys
-clr.AddReference('ProtoGeometry')
 
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
-appDataPath = os.getenv('APPDATA')
-msPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\extra"
-rhPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin"
-rhDllPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll"
-if msPath not in sys.path:
-	sys.path.Add(msPath)
-if rhPath not in sys.path:
-	sys.path.Add(rhPath)
-	clr.AddReferenceToFileAndPath(rhDllPath)
 
-from Autodesk.DesignScript.Geometry import *
+appDataPath = os.getenv('APPDATA')
+rhDllPath = appDataPath + r'\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll'
+clr.AddReferenceToFileAndPath(rhDllPath)
+
 import Rhino as rc
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
-rhObjects = IN[0]
 
-#get object guid
-rhGUID = []
-for i in rhObjects:
-	rhGUID.append(i.Attributes.ObjectId)
-	
+if isinstance(IN[0], list):
+	rhObjects = IN[0]
+else:
+	rhObjects = [IN[0]]
+
+def GetObjectGUID(obj):
+	return obj.Attributes.ObjectId
+
+def ProcessList(_func, _list):
+	return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
+
+#extract object type
+try:
+	errorReport = None
+	rhGUID = ProcessList(GetObjectGUID, rhObjects)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+
 #Assign your output to the OUT variable
-OUT = rhGUID
+if errorReport == None:
+	OUT = rhGUID
+else:
+	OUT = errorReport
