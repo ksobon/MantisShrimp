e84970b5dddb36192e3eb64019a1e6d3fa62aca7
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-23 22:55:13 -0400

    Update Get Rhino Object Name.py

--- Dynamo/src/Get Rhino Object Name.py
+++ Dynamo/src/Get Rhino Object Name.py
@@ -1,35 +1,45 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
 import sys
-clr.AddReference('ProtoGeometry')
 
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
-appDataPath = os.getenv('APPDATA')
-msPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\extra"
-rhPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin"
-rhDllPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll"
-if msPath not in sys.path:
-	sys.path.Add(msPath)
-if rhPath not in sys.path:
-	sys.path.Add(rhPath)
-	clr.AddReferenceToFileAndPath(rhDllPath)
 
-from Autodesk.DesignScript.Geometry import *
+appDataPath = os.getenv('APPDATA')
+rhDllPath = appDataPath + r'\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll'
+clr.AddReferenceToFileAndPath(rhDllPath)
+
 import Rhino as rc
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
-rhObjects = IN[0]
 
-#get object names
-rhNames = []
-for i in rhObjects:
-	rhNames.append(i.Name)
-	
+if isinstance(IN[0], list):
+	rhObjects = IN[0]
+else:
+	rhObjects = [IN[0]]
+
+def GetObjectName(obj):
+	return obj.Attributes.Name
+
+def ProcessList(_func, _list):
+	return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
+
+#extract object type
+try:
+	errorReport = None
+	rhNames = ProcessList(GetObjectName, rhObjects)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+
 #Assign your output to the OUT variable
-OUT = rhNames
+if errorReport == None:
+	OUT = rhNames
+else:
+	OUT = errorReport
