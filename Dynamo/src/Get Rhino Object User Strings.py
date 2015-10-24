33a20df75b8aa9559495bc797cd94bfa30732336
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-23 22:56:24 -0400

    Update Get Rhino Object User Strings.py

--- Dynamo/src/Get Rhino Object User Strings.py
+++ Dynamo/src/Get Rhino Object User Strings.py
@@ -1,46 +1,65 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
 import sys
-clr.AddReference('ProtoGeometry')
 
 pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
-appDataPath = os.getenv('APPDATA')
-msPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\extra"
-rhPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin"
-rhDllPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll"
-if msPath not in sys.path:
-	sys.path.Add(msPath)
-if rhPath not in sys.path:
-	sys.path.Add(rhPath)
-	clr.AddReferenceToFileAndPath(rhDllPath)
 
-from Autodesk.DesignScript.Geometry import *
+appDataPath = os.getenv('APPDATA')
+rhDllPath = appDataPath + r'\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll'
+clr.AddReferenceToFileAndPath(rhDllPath)
+
 import Rhino as rc
 
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
-rhObjects = IN[0]
-keys = IN[1]
 
-#extract all user strings from rhino object
-try:
-	item = item.Geometry
-except:
-	pass
-if not any(isinstance(item, list) for item in keys):
-	userStrings = []
-	for key in keys[index]:
-		userStrings[index].extend(item.GetUserStrings().GetValues(key))
+if isinstance(IN[0], list):
+	rhObjects = IN[0]
 else:
-	userStrings = [[] for i in range(len(rhObjects))]
-	for index, item in enumerate(rhObjects):
-		for key in keys[index]:
-			userStrings[index].extend(item.GetUserStrings().GetValues(key))
+	rhObjects = [IN[0]]
+
+def GetObjectKeys(obj):
+	try:
+		keys = []
+		if obj.Attributes.HasUserData:
+			for i in range(0, obj.Attributes.GetUserStrings().Keys.Count, 1):
+				key = obj.Attributes.GetUserStrings().Keys.Item[i]
+				keys.append(key)
+			return keys
+	except:
+		pass
+
+def GetObjectValues(obj):
+	try:
+		values = []
+		if obj.Attributes.HasUserData:
+			for i in range(0, obj.Attributes.GetUserStrings().Keys.Count, 1):
+				key = obj.Attributes.GetUserStrings().Keys.Item[i]
+				values.append(obj.Attributes.GetUserStrings().Item[key])
+			return values
+	except:
+		pass
+
+def ProcessList(_func, _list):
+	return map( lambda x: ProcessList(_func, x) if type(x)==list else _func(x), _list )
+
+#extract user keys
+try:
+	errorReport = None
+	rhKeys = ProcessList(GetObjectKeys, rhObjects)
+	rhValues = ProcessList(GetObjectValues, rhObjects)
+except:
+	# if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
 
 #Assign your output to the OUT variable
-OUT = userStrings
+if errorReport == None:
+	OUT = rhKeys, rhValues
+else:
+	OUT = errorReport
