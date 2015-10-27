345bfdeeceb84c7505fdd534cbb36f0b7572ae7d
Konrad K Sobon <ksobon1986@gmail.com>
2015-10-27 10:45:09 -0400

    Update Get Rhino File.py

--- Dynamo/src/Get Rhino File.py
+++ Dynamo/src/Get Rhino File.py
@@ -1,4 +1,4 @@
-#Copyright(c) 2015, Konrad Sobon
+# Copyright(c) 2015, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net
 
 import clr
@@ -9,15 +9,27 @@ pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
 sys.path.append(pyt_path)
 
 import os
+test = []
 appDataPath = os.getenv('APPDATA')
-msPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\extra"
-rhPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin"
-rhDllPath = appDataPath + r"\Dynamo\0.8\packages\Mantis Shrimp\bin\Rhino3dmIO.dll"
-if msPath not in sys.path:
-	sys.path.Add(msPath)
-if rhPath not in sys.path:
-	sys.path.Add(rhPath)
-	clr.AddReferenceToFileAndPath(rhDllPath)
+dynVersions = ["0.7", "0.8", "0.9"]
+dynFolders = ["extra", "bin"]
+for i in dynVersions:
+	for j in dynFolders:
+		part1 = "\\Dynamo\\"
+		version = i
+		part2 = "\\packages\\Mantis Shrimp\\"
+		folder = j
+		msPath = part1 + version + part2 + folder
+		if j == "extra":
+			if msPath not in sys.path:
+				sys.path.Add(msPath)
+		if j == "bin":
+			dllName = "\\Rhino3dmIO.dll"
+			rhDllPath = part1 + version + part2 + folder + dllName
+			if msPath not in sys.path:
+				sys.path.Add(msPath)
+				if os.path.isfile(rhDllPath):
+					clr.AddReferenceToFileAndPath(rhDllPath)
 
 from Autodesk.DesignScript.Geometry import *
 import Rhino as rc
@@ -26,6 +38,5 @@ import Rhino as rc
 dataEnteringNode = IN
 path = str(IN[0])
 
-#read 3dm file
 #Assign your output to the OUT variable
 OUT = rc.FileIO.File3dm.Read(path)
